61955048c2f3000f3fcb2c4778ac9a43c5132060
anegerega <angelaegerega@yahoo.com>
2020-02-06 12:08:20 +0100

    Modified OddNumbers Program, Added program to check if element occurs in a list

---
+++ ElementOccurence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElementOccurence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list1 = new List<string>()
+            {
+                "Angela",
+                "Nwazuosa",
+                "Egerega",
+                "Mary",
+                "Elizabeth"
+
+            };
+            Console.Write("List...\n");
+            foreach (string list in list1)
+            {
+                Console.WriteLine(list);
+            }
+
+            Console.WriteLine("Find this item in the list");
+            string userItem =  Console.ReadLine();
+            if (list1.Contains(userItem) == true)
+            {
+                Console.WriteLine("Item exists!");
+            }
+            else
+            {
+                Console.WriteLine("Item does not exist!");
+            }
+        }
+    }
+}
