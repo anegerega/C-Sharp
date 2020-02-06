664e856aefc5d5f852aaa1e6ccc4528a55ef96a0
anegerega <angelaegerega@yahoo.com>
2020-02-06 13:30:57 +0100

    Program to check if a string is Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Type a word:");
+            string word = Console.ReadLine();
+            char[] newWord = word.ToCharArray();
+            Array.Reverse(newWord);
+            string reversedWord = new string(newWord);
+            if (reversedWord == word)
+            {
+                Console.Write("It is");
+            }
+            else
+            {
+                Console.Write("It is NOT");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
