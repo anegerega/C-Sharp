04e6bac42cc98fc06a55b78c3bd83cdb511bdb00
anegerega <angelaegerega@yahoo.com>
2020-02-11 13:20:48 +0100

    made Palindrome code more dynamic

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -4,23 +4,38 @@ namespace Palindrome
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            AskAgain:
             Console.Write("Type a word:");
             string word = Console.ReadLine();
             char[] newWord = word.ToCharArray();
             Array.Reverse(newWord);
             string reversedWord = new string(newWord);
-            if (reversedWord == word)
+            string upreservedword = reversedWord.ToUpper();
+            string upword = word.ToUpper();
+            if (upreservedword == upword)
             {
-                Console.Write("It is");
+                Console.WriteLine("Yes, this word is Palindrome!");
+                goto Continueasking;
             }
             else
             {
-                Console.Write("It is NOT");
+                Console.WriteLine("Sorry, this word is not Palindrome");
+                goto Continueasking;
             }
-
+            Continueasking:
+            Console.WriteLine("Do you want to continue? (Enter Yes or No)");
+            string decide = Console.ReadLine();
+            string decided = decide.ToUpper();
+            if (decided == "YES")
+            {
+                goto AskAgain;
+            } else 
+            if (decided == "NO")
+            {
+                Console.WriteLine("Thank you for banking with us!");
+            }
             Console.ReadLine();
         }
     }
