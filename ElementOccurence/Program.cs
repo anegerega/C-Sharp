e235c6b6d00b0aac7dfd283c167c2b818de15df7
anegerega <angelaegerega@yahoo.com>
2020-02-18 12:37:59 +0100

    modified Element Occurence in a list

--- ElementOccurence/Program.cs
+++ ElementOccurence/Program.cs
@@ -5,29 +5,37 @@ namespace ElementOccurence
 {
     class Program
     {
+        public static string YourItem { get; private set; }
+
         static void Main(string[] args)
         {
-            AskAgain:
+        AskAgain:
             Console.WriteLine("Enter your list of items and separate each item with a comma (,): ");
             string new_string = Console.ReadLine();
-            string[] YourList = new string[] {" "};
+            string[] YourList = new string[] { " " };
             YourList = new_string.Split(',');
 
             Console.Write("Now tell us which item are you looking for: ");
             string word = Console.ReadLine();
-            
+
             foreach (string YourItem in YourList)
             {
-                Console.Write(YourItem + " ");
                 if (YourItem == word)
                 {
-                    Console.WriteLine("Found");
-                }
-                else
-                {
-                    Console.WriteLine("-");
+                    Console.WriteLine(word + " exists in your list");
                 }
             }
+
+            int testfound = 0;
+            foreach(string i in YourList)
+            {
+                if(i == word)
+                {
+                    testfound = +1; 
+                }
+            }
+            if (testfound == 0) Console.WriteLine("Entry not found.");
+
             Console.WriteLine("Do you want to search through another list? Enter Yes or No");
             string decide = Console.ReadLine().ToUpper();
             if(decide == "YES")
@@ -40,4 +48,6 @@ namespace ElementOccurence
             }
         }
     }
+
 }
+
