b40ca7c0bf1dff350bc9d583dd7336291ae76cec
anegerega <angelaegerega@yahoo.com>
2020-02-18 13:19:11 +0100

    modified ListSum using For Loop and While Loop

--- ListSum/Program.cs
+++ ListSum/Program.cs
@@ -7,41 +7,46 @@ namespace ListSum
     {
         static void Main(string[] args)
         {
-            While_Loop();             
+            For_Loop();             
         }
         public static void For_Loop() // using for loop to add elements in an array
         {
-            int[] myArray = new int[10000];
-            int i, n, sum = 0;
 
-            Console.WriteLine("Find sum of all elements in array");
-
-            Console.Write("Enter the number of elements to be stored in the array :");
-            n = Convert.ToInt32(Console.ReadLine());
-
+            Console.WriteLine("Find sum of all items in array");
+            Console.Write("Enter the number of items to be stored in the array :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int[n];
+            int i = 0;
+            int sum = 0;
             for (i = 0; i < n; i++)
             {
-                Console.Write("element - {0} : ", i);
-                myArray[i] = Convert.ToInt32(Console.ReadLine());
-                sum += myArray[i];
+                if (i < n)
+                {
+                    Console.Write("element - {0} : ", i);
+                    myArray[i] = Convert.ToInt32(Console.ReadLine());
+                    sum += myArray[i];
+                }               
             }
             Console.Write("Sum of all elements stored in the array is : " + sum);
         }
         public static void While_Loop() //using while loop to add up elements in an array
         {
-            int[] myArray = new int[10000];
-            int i, n, sum = 0;
 
             Console.WriteLine("Find sum of all items in array");
 
             Console.Write("Enter the number of items to be stored in the array :");
-            n = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());          
+            int[] myArray = new int[n];
+            int i = 0;
+
+            int sum = 0;
             while (i < n)
             {
                 Console.Write("item - {0} : ", i);
-                i++;
+                
                 myArray[i] = Convert.ToInt32(Console.ReadLine());
                 sum += myArray[i];
+                i++;
             }
             Console.Write("Sum of all elements stored in the array is : " + sum);
         }
