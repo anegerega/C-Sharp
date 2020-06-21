27bf46fd5fa45e6d4787c5bf14c0420f8b3f5678
Iamkemical1 <iamkemical1@gmail.com>
2020-06-21 17:07:59 +0100

    Changes made on the connection string

--- Services/Startup.cs
+++ Services/Startup.cs
@@ -27,11 +27,9 @@ namespace Services
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<UserDbContext>();
             services.AddControllers();
             services.AddDbContextPool<UserDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("ApplicationConnection")));
-            services.AddScoped<UserDbContext, UserDbContext>();
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
